5bef03f41e11bf616cb8ba7c1d705bab3d41727e
M.Voropaev <maks.woropaew@mail.ru>
2022-07-07 11:07:27 +0300

    nakonec sdelal

--- tesk1/Program.cs
+++ tesk1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+//4  -> 24
+//5  -> 120
+
+//2. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] array = new int[8];
+           for (int i = 0; i < array.Length;i++)
+           {
+            array[i] = new Random().Next(0,2);
+            Console.Write(array[i]);
+           }              
+        
+        }
+    }
+}
